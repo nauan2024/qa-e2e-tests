befda36ad706a89887f853d9cf4ec6de8c912b11
nauan2024 <nauanlaurentinotec@gmail.com>
2026-03-27 17:43:31 -0300

    feat:formulario update

--- web/cypress/e2e/Formulario.cy.js
+++ web/cypress/e2e/Formulario.cy.js
@@ -45,47 +45,17 @@ describe('template spec', () => {
 
     cy.get("#details").type("Parabens acabou de funcionar, se leu me deve um bis")
 
-    cy.contains("input")
+    cy.get("#technologies").type("contratulations").type("{enter}").should("have.value", "")
+
+    cy.contains("label", "Li e aceito os termos de uso").find("input").should("be.visible").check()
+
+    cy.contains("button", "Enviar formulário").click()
+
+
+
+   // cy.contains("input")
 
 
     cy.wait(20000)
-  })
-
-  it("Login cpf", () => {
-        cy.visit('http://localhost:3000/')
-
-    cy.get('#email').type("4dt@gmail.com")
-    cy.get("#password").type("4DT")
-
-    cy.contains("button", "Entrar").click()
-
-    cy.contains("h4", "Formulários").click()
-
-    cy.get("#name").type("Nauan Laurentino Silva")
-
-    cy.get("#email").type("Nauan@2023")
-
-    cy.get("#phone").type("11999990000").should("have.value", "(11) 99999-0000")
-    //escolhe a formatação exata do telefone, se não estiver exatamente assim não será aceito
-
-    cy.get("#consultancyType").select("In Company")
-
-    cy.contains("span", "Pessoa Física").click()
-
-    cy.contains("span", "Pessoa Jurídica").should("be.not.checked")
-
-    cy.get("#document").type("94419337400").should("have.value", "944.193.374-00")
-    //escolhe a formatação exata do cpf, se não estiver exatamente assim não será aceito
-
-   
-    
-
-    cy.get("technologies").type("Abacom").should("have.value", "Abacom").type("{enter}")
-
-
-    cy.wait(2000)
-
-
-
   })
 })
