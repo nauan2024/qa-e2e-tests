1769c90df36658fa4162802921da3aaf7ff66a0f
nauan2024 <nauanlaurentinotec@gmail.com>
2026-03-27 17:18:21 -0300

    feat: fiama

---
+++ web/cypress/e2e/Formulario.cy.js
@@ -0,0 +1,91 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('http://localhost:3000/')
+
+    cy.get('#email').type("4dt@gmail.com")
+    cy.get("#password").type("4DT")
+
+    cy.contains("button", "Entrar").click()
+
+    cy.contains("h4", "Formulários").click()
+
+    cy.get("#name").type("Nauan Laurentino Silva")
+
+    cy.get("#email").type("Nauan@2023")
+
+    cy.get("#phone").type("11999990000").should("have.value", "(11) 99999-0000")
+    //escolhe a formatação exata do telefone, se não estiver exatamente assim não será aceito
+
+    cy.get("#consultancyType").select("In Company")
+
+    cy.contains("span", "Pessoa Jurídica").click()
+
+    cy.contains("Label", "Pessoa Física", {timeout: 10000}).find("input").should("be.not.checked")
+    //validação para garantir que o campo "Pessoa Física" esteja desmarcado após clicar em "Pessoa Jurídica"
+
+    cy.get("#document").type("12345678000100").should("have.value", "12.345.678/0001-00") 
+
+    cy.contains("label", "Instagram").find("input").check().should("be.checked")
+
+    const redes = [
+      "Instagram",
+      "LinkedIn",
+      "Udemy",
+      "YouTube",
+      "Indicação de Amigo",
+    ]
+
+    redes.forEach((item1)=>{
+      return(
+        cy.contains("label", item1).find("input").check().should("be.checked")
+      )
+    })
+
+    cy.get('input[type="file"]').selectFile("./cypress/fixtures/header.png", {force: true})
+
+    cy.get("#details").type("Parabens acabou de funcionar, se leu me deve um bis")
+
+    cy.contains("input")
+
+
+    cy.wait(20000)
+  })
+
+  it("Login cpf", () => {
+        cy.visit('http://localhost:3000/')
+
+    cy.get('#email').type("4dt@gmail.com")
+    cy.get("#password").type("4DT")
+
+    cy.contains("button", "Entrar").click()
+
+    cy.contains("h4", "Formulários").click()
+
+    cy.get("#name").type("Nauan Laurentino Silva")
+
+    cy.get("#email").type("Nauan@2023")
+
+    cy.get("#phone").type("11999990000").should("have.value", "(11) 99999-0000")
+    //escolhe a formatação exata do telefone, se não estiver exatamente assim não será aceito
+
+    cy.get("#consultancyType").select("In Company")
+
+    cy.contains("span", "Pessoa Física").click()
+
+    cy.contains("span", "Pessoa Jurídica").should("be.not.checked")
+
+    cy.get("#document").type("94419337400").should("have.value", "944.193.374-00")
+    //escolhe a formatação exata do cpf, se não estiver exatamente assim não será aceito
+
+   
+    
+
+    cy.get("technologies").type("Abacom").should("have.value", "Abacom").type("{enter}")
+
+
+    cy.wait(2000)
+
+
+
+  })
+})
